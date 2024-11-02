7c095507f5848f0df0f50482a7195edd88b780be
arifalfazhar <rokokksp03@gmail.com>
2024-11-01 22:52:11 -0700

    03 nilai default argumen .php

---
+++ 03 nilai default argumen .php
@@ -0,0 +1,8 @@
+
+<?php
+function nama_bulan($bulan, $tahun = 2013) {
+	echo $bulan . ' ' . $tahun;
+}
+nama_bulan('november'); // Hasil Januari 2013
+
+echo "<p><i><strong>By:arif alfazhar</strong>";
