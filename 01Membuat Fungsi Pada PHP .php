fcf365b7a4618148ad34057a66644adadb97ee9b
arifalfazhar <rokokksp03@gmail.com>
2024-11-01 22:49:17 -0700

    01Membuat Fungsi Pada PHP .php

---
+++ 01Membuat Fungsi Pada PHP .php
@@ -0,0 +1,16 @@
+<?php
+// mmbuat fungsi
+function perkenalan(){
+  echo "Assalamulaikum, ";
+  echo "Perkenalkan, nama arif alfazhar<br/>";
+  echo "Senang berkenalan dengan kalian semua<br/>";
+}
+// memanggil fungsi yang sudah dibuat
+perkenalan();
+
+echo "<hr>";
+// memanggilnya lagi
+perkenalan();
+
+echo "<p><i><strong>By:arif alfazhar</strong>";
+?>
