e405f7e04f9471e80996fc471e35943acbc401c1
arifalfazhar <rokokksp03@gmail.com>
2024-11-17 18:23:40 -0800

    16 sintaks argumen.php

---
+++ 16 sintaks argumen.php
@@ -0,0 +1,10 @@
+
+<?php
+function sapa($nama = 'pengunjung') {
+    echo "Halo, $nama!";
+}
+
+sapa(); // Output: Halo, pengunjung!
+sapa('Amellya'); // Output: Halo, ipal!
+
+echo "<p><strong>by arif alfazhar</strong>";
