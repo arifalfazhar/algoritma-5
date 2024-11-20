4bd2ea441ef1a1a790405733584baf872c1dd499
arifalfazhar <rokokksp03@gmail.com>
2024-11-19 19:28:53 -0800

    38 header location.php

---
+++ 38 header location.php
@@ -0,0 +1,13 @@
+<?php
+header("Location:http://localhost/arif/algoritma%205/20%20DATE%20FUNCITION.php");
+exit; // Selalu panggil exit setelah header location untuk menghentikan eksekusi skrip
+header("Content-Type: application/pdf");
+// Kode untuk mengirimkan file PDF
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
+
+
+echo "<p><strong>by arif alfazhar</strong>";
+
+?>
