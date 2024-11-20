8a8eb36d9d0024176a77459fed25d75fcd1ac69d
arifalfazhar <rokokksp03@gmail.com>
2024-11-19 20:30:32 -0800

    membuat proses perkenalan.php

---
+++ membuat proses perkenalan.php
@@ -0,0 +1,16 @@
+<?php
+// mmbuat fungsi
+function perkenalan(){
+  echo "Assalamulaikum, ";
+  echo "Perkenalkan, nama saya arif<br/>";
+  echo "Saya murid smkn 1 karang baru<br/>";
+  echo "Umur saya 17 tahun<br/>";
+  echo "Senang berkenalan dengan Teman-teman semua<br/>";
+}
+
+
+// memanggilnya lagi
+perkenalan();
+
+echo "<p><strong>by arif alfazhar</strong>";
+?>
