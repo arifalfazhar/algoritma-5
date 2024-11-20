8fd1b376f4d69573fac87b6cb36042697af9451d
arifalfazhar <rokokksp03@gmail.com>
2024-11-19 19:47:30 -0800

    49 membuat form html.php

---
+++ 49 membuat form html.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<form method="post" action="membuat proses perkenalan.php">
+  Nama: <input type="text" name="nama" required><br>
+  Email: <input type="email" name="email" required><br>
+
+  <input type="submit" value="Kirim">
+</form>
+
+<p><strong>by arif alfazhar</strong>
