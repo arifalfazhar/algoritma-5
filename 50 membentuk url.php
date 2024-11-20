3c4610b6e671941c8e17f0c7348561f3de8d88e6
arifalfazhar <rokokksp03@gmail.com>
2024-11-19 19:51:49 -0800

    50 membentuk url.php

---
+++ 50 membentuk url.php
@@ -0,0 +1,6 @@
+<!DOCTYPE HTML>
+<form action="membuat formulir metode get.php" method="get">
+  Cari: <input type="text" name="query"><br>
+  <input type="submit" value="Search">
+</form>
+<p><strong>by arif alfazhar</strong>
