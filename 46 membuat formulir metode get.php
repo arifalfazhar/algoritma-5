88f95c8e194762dc8064899935e043f1e42f0c98
arifalfazhar <rokokksp03@gmail.com>
2024-11-19 19:43:19 -0800

    46 membuat formulir metode get.php

---
+++ 46 membuat formulir metode get.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contoh Formulir GET di PHP</title>
+</head>
+<body>
+
+<form action="menggunakan method post.php" method="get">
+    Nama: <input type="text" name="nama"><br>
+    Email: <input type="text" name="email"><br>
+    <input type="submit" value="Kirim">
+
+    <p><strong>by arif alazhar</strong>
+</form>
+</body>
+</html>
