f546b0f0338b00a96d4a991e85b2299ba8454e2a
arifalfazhar <rokokksp03@gmail.com>
2024-11-19 19:54:40 -0800

    52 memulai dengan html form.php

---
+++ 51 KARAKTER.php
@@ -0,0 +1,5 @@
+<?PHP
+$param = 'makanan & minuman';
+echo '<a href="membuat proses perkenalan.php?category='. urlencode($param) .'">Link</a>';
+
+echo "<p><strong>By Amellya</strong>";
---
+++ 52 memulai dengan html form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<form method="post" action="membuat proses perkenalan.php">
+    <label for="name">Nama:</label>
+    <input type="text" id="name" name="name"><br>
+    
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email"><br>
+    
+    <label for="message">Pesan:</label>
+    <textarea id="message" name="message"></textarea><br>
+    
+    <input type="submit" value="Kirim">
+</form>
+
+
+<p><strong>by arif alfazhar</strong>
